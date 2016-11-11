f7d3234e41c401c77a5119fe7a4979cb2267b72f
Aron <aronmn@vgy.se>
2016-11-11 14:51:05 +0100

    Övning 1

--- metoder-funktioner/Program.cs
+++ metoder-funktioner/Program.cs
@@ -8,8 +8,16 @@ namespace metoder_funktioner
 {
     class Program
     {
+        public static void CelsiusTillFarenheit(double Celsius)
+        {
+            Console.WriteLine((Celsius/5.0)*9+32);
+        }
         static void Main(string[] args)
         {
+            //Övning 1
+            Console.WriteLine("Mata in antal grader i Celsius så omvandlar programmet det till Farenheit");
+            double Celsius = double.Parse(Console.ReadLine());
+            CelsiusTillFarenheit(Celsius);
         }
     }
 }
