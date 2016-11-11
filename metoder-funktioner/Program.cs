f5c2b738cae676b057670d19f8e6184d3d54deb4
Aron <aronmn@vgy.se>
2016-11-11 15:18:38 +0100

    Övning 1-2

--- metoder-funktioner/Program.cs
+++ metoder-funktioner/Program.cs
@@ -8,16 +8,42 @@ namespace metoder_funktioner
 {
     class Program
     {
-        public static void CelsiusTillFarenheit(double Celsius)
+            static void CelsiusTillFarenheit(double Celsius)
         {
             Console.WriteLine((Celsius/5.0)*9+32);
         }
+
+            static double RaknaArea(double Radie)
+        {
+            return(Math.PI*Radie*Radie);
+        }
+
+            static double RaknaOmkrets(double Radie)
+        {
+            return(Radie * 2) * Math.PI;
+        }
+
+
         static void Main(string[] args)
         {
             //Övning 1
+
             Console.WriteLine("Mata in antal grader i Celsius så omvandlar programmet det till Farenheit");
             double Celsius = double.Parse(Console.ReadLine());
             CelsiusTillFarenheit(Celsius);
+
+            //Övning 2
+
+            Console.WriteLine("Mata in cirkelns radie:");
+            double Radie = double.Parse(Console.ReadLine());
+            Console.WriteLine("Arean på cirkeln är:");
+            Math.Round(RaknaArea(Radie),2);
+            Console.WriteLine(RaknaArea(Radie));
+
+            Console.Write("Omkresten är:");
+            Math.Round (RaknaOmkrets(Radie),2);
+            Console.WriteLine(RaknaOmkrets(Radie));
+
         }
     }
 }
